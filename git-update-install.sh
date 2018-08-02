b8216bfa18a1ca3cc86c25314c94b23ce55918ba
clinch123 <41845948+clinch123@users.noreply.github.com>
2018-08-02 10:40:01 -0500

    Update git-update-install.sh

--- git-update-install.sh
+++ git-update-install.sh
@@ -1,1 +1,25 @@
+#!/bin/bash
 
+echo "Checking if program is running with sudo privileges..."
+#check if running as root
+if [ "$EUID" -ne 0 ]
+  then echo "Please run with sudo access."
+  exit
+fi
+echo "Program is running with sudo privileges." 
+
+echo "Checking if git-update.sh has been downloaded"
+#Check if git-update.sh exists in /opt/git-update
+if [ ! -f /opt/git-update/git-update.sh ]; then
+  echo "File not found. Downloading script"
+  if [ ! -d /opt/git-update/ ]; then
+    mkdir /opt/gitupdate/
+  fi
+  sudo git clone
+else
+
+fi
+
+#Check if git-update.sh is executable
+
+#Check if ln exists between /opt/git-update/git-update.sh and /usr/local/bin/git-update
