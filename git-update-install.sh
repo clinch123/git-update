c0a302dc16fb60bc39b99e27ab926cad8da35cfe
clinch123 <41845948+clinch123@users.noreply.github.com>
2018-08-07 09:09:30 -0500

    Update git-update-install.sh

--- git-update-install.sh
+++ git-update-install.sh
@@ -17,10 +17,25 @@ if [ ! -f /opt/git-update/git-update.sh ]; then
   fi
   cd /opt/git-update
   sudo git clone https://github.com/clinch123/git-update.git
+  if [ ! -f /opt/git-update/git-update.sh ]; then
+    echo "Unable to download git-update.sh. Please troubleshoot further"
+    exit
+  else
+    echo "Git-Update downloaded. Continuing installation."
+  fi
 else
-
+  echo "Git-update is correctly downloaded. Continuing installation."
 fi
 
 #Check if git-update.sh is executable
-
+echo "Checking if git-update.sh is executeable."
+if [ ! -x /opt/git-update/git-update.sh ]; then
+  sudo chmod +x /opt/git-update/git-update.sh
+  if [ ! -x /opt/git-update/git-update.sh ]; then
+    echo "Unable to make file executeable. Please troubleshoot further"
+    exit
+  fi
+  echo "git-update.sh is executable. Continuing..."
+fi
+  
 #Check if ln exists between /opt/git-update/git-update.sh and /usr/local/bin/git-update
